37d4b60864a01c0f029b0f2d4b92ebb31c3bf61c
ZcoolZBrony <50166062+ZcoolZBrony@users.noreply.github.com>
2019-05-01 16:03:32 +0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+client.on('message', msg => {
+  if (msg.content === '!hai') {
+    msg.reply('Hey too There1')
+  }
+})
+
+client.on('guildMemberAdd', member => {
+  member.send(`Welcome on the server! Please be aware that we won’t tolerate troll, spam or harassment. Have fun`)
+})
+
+client.on('message', message => {
+  if (message.content.startsWith('!kick')) {
+    const member = message.mentions.members.first()
+    
+    if (!member) {
+      return message.reply(`Who are you trying to kick? You must mention a user.`)
+    }
+    if (!member.kickable) {
+      return message.reply(`I can't kick this user. Sorry!`)
+    }
+    return member
+      .kick()
+      .then(() => message.reply(`${member.user.tag} was kicked.`))
+      .catch(error => message.reply(`Sorry, an error occured.`))
+  }
+})
+
+
+client.login('process.env.BOT_TOKEN')
